f538e19d3fa38c78822adbf1db5c8625810cbe9b
Wayne Quinlivan <x@gmail.com>
2018-01-06 17:23:02 +0000

    Refactor AccountTest

--- src/test/java/com/sg/AccountTests.java
+++ src/test/java/com/sg/AccountTests.java
@@ -1,15 +1,30 @@
 package com.sg;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("An Account")
 public class AccountTests {
 
-    @Test
-    @DisplayName("should initialise with a zero balance")
-    void shouldInitialiseWithZeroBalance() {
-        assertEquals(0, new Account("Harry").getBalance());
+    @Nested
+    @DisplayName("when new")
+    class WhenNew {
+
+        Account account;
+
+        @BeforeEach
+        void createNewAccount() {
+            account = new Account("Harry");
+        }
+
+        @Test
+        @DisplayName("should initialise with a zero balance")
+        void shouldInitialiseWithZeroBalance() {
+            assertEquals(0, account.getBalance());
+        }
     }
 }
