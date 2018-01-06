00fb33279e58a52b939d5f96d32eb6b8817ebfb3
Wayne Quinlivan <x@gmail.com>
2018-01-06 15:59:11 +0000

    Add test for Account initial conditions

--- src/main/java/com/sg/Account.java
+++ src/main/java/com/sg/Account.java
@@ -1,4 +1,28 @@
 package com.sg;
 
+/**
+ * This class is responsible for maintaining the list of accounting entries.
+ */
 public class Account {
+
+    private String id;
+    private double balance;
+
+    /**
+     * Class cannot be constructed from the default constructor
+     */
+    private Account() {
+    }
+
+    /**
+     * Class constructor specifying metadata for the account.
+     * @param id the id of the account
+     */
+    public Account(String id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/test/java/com/sg/AccountTests.java
+++ src/test/java/com/sg/AccountTests.java
@@ -1,11 +1,15 @@
 package com.sg;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTests {
 
     @Test
-    void replaceMe() {
-
+    @DisplayName("should initialise with a zero balance")
+    void shouldInitialiseWithZeroBalance() {
+        assertEquals(0, new Account("Harry").getBalance());
     }
 }
