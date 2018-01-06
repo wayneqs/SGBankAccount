3fdcc75a4d32e1d7bc741d5f67ef23d2ad0274fe
Wayne Quinlivan <x@gmail.com>
2018-01-06 22:23:54 +0000

    Add support for finding entries by counterparty

--- src/main/java/com/sg/Account.java
+++ src/main/java/com/sg/Account.java
@@ -1,6 +1,12 @@
 package com.sg;
 
+import javax.swing.text.html.Option;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This class is responsible for maintaining the list of accounting entries.
@@ -9,6 +15,7 @@ public class Account {
 
     private String id;
     private BigDecimal balance;
+    private List<Entry> entries = new ArrayList<>();
 
     /**
      * Class cannot be constructed from the default constructor
@@ -31,13 +38,17 @@ public class Account {
 
     public void credit(Entry entry) {
         this.balance = this.balance.add(entry.getAmount());
+        this.entries.add(entry);
     }
 
     public void debit(Entry entry) {
         this.balance = this.balance.subtract(entry.getAmount());
+        this.entries.add(entry);
     }
 
-    public Entry findByCounterPartyId(String counterpartyId) {
-        return null;
+    public List<Entry> findByCounterPartyId(String counterpartyId) {
+        return entries.stream()
+                .filter(entry -> entry.getCounterparty().equals(counterpartyId))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/sg/AccountTests.java
+++ src/test/java/com/sg/AccountTests.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @DisplayName("An Account")
@@ -75,27 +78,47 @@ class AccountTests {
         Entry entry1;
         Entry entry2;
         Entry entry3;
-        BigDecimal amount1 = BigDecimal.valueOf(1.1);
-        BigDecimal amount2 = BigDecimal.valueOf(2.2);
-        BigDecimal amount3 = BigDecimal.valueOf(3.3);
+        Entry entry4;
 
         @BeforeEach
         void addSeveralEntries() {
-            entry1 = mock(Entry.class);
-            entry2 = mock(Entry.class);
-            entry3 = mock(Entry.class);
-            when(entry1.getAmount()).thenReturn(amount1);
-            when(entry2.getAmount()).thenReturn(amount2);
-            when(entry3.getAmount()).thenReturn(amount3);
+            entry1 = createMockEntry("counterparty1", BigDecimal.valueOf(1.1));
+            entry2 = createMockEntry("counterparty2", BigDecimal.valueOf(2.2));
+            entry3 = createMockEntry("counterparty3", BigDecimal.valueOf(3.3));
+            entry4 = createMockEntry("counterparty3", BigDecimal.valueOf(1));
             account.credit(entry1);
             account.debit(entry2);
             account.credit(entry3);
+            account.credit(entry4);
         }
 
         @Test
-        @DisplayName("the balance should equal the amount of the entry")
+        @DisplayName("the balance should equal the accounting sum of the entries")
         void shouldEqualTheEntry() {
-            assertEquals(amount1.subtract(amount2).add(amount3), account.getBalance());
+            assertEquals(BigDecimal.valueOf(3.2), account.getBalance());
         }
+
+        @Test
+        @DisplayName("the account should return null for counterparty search misses")
+        void shouldReturnEmptyForEntryMatchMisses() {
+            assertTrue(account.findByCounterPartyId("no-entries-for-this-counterparty").isEmpty());
+        }
+
+        @Test
+        @DisplayName("the account should return all matches for counterparty search")
+        void shouldReturnMatchesForEntryMatchHits() {
+            List<Entry> matches = account.findByCounterPartyId(entry3.getCounterparty());
+            assertAll("found entries",
+                        () -> assertTrue(matches.size() == 2),
+                        () -> assertTrue(matches.contains(entry3)),
+                        () -> assertTrue(matches.contains(entry4)));
+        }
+    }
+
+    private Entry createMockEntry(String counterpartyId, BigDecimal amount) {
+        Entry entry = mock(Entry.class);
+        when(entry.getCounterparty()).thenReturn(counterpartyId);
+        when(entry.getAmount()).thenReturn(amount);
+        return entry;
     }
 }
