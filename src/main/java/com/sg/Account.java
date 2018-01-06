452ef1c72020ae04c85ac35218f2d9c5c385b547
Wayne Quinlivan <x@gmail.com>
2018-01-06 14:30:17 +0000

    Add Account object model

---
+++ src/main/java/com/sg/Account.java
@@ -0,0 +1,4 @@
+package com.sg;
+
+public class Account {
+}
---
+++ src/test/java/com/sg/AccountTests.java
@@ -0,0 +1,11 @@
+package com.sg;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    void replaceMe() {
+
+    }
+}
